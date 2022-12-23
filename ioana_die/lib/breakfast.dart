61c273d44aec19ccc5d0be5acaef1d7fd9e1eda4
NabeelAzuhar <nabeel_azuhar@yahoo.com.sg>
2022-12-23 01:05:12 +0000

    Changed layout
    Changed from table to scrollable card layout

--- ioana_die/lib/breakfast.dart
+++ ioana_die/lib/breakfast.dart
@@ -1,36 +1,175 @@
 import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
 
 class BreakfastPage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Center(
-      child: Column(children: [
-        Text(
-          'BREAKFAST',
-          style: TextStyle(fontSize: 24, fontWeight: FontWeight.bold),
+        child: Column(
+      children: [
+        const Padding(
+          padding: EdgeInsets.all(8.0),
+          child: Text(
+            'BREAKFAST',
+            style: TextStyle(
+              fontSize: 24,
+              fontWeight: FontWeight.bold,
+            ),
+          ),
         ),
         Container(
-          decoration: BoxDecoration(
-              borderRadius: BorderRadius.circular(20),
-              color: CupertinoColors.systemBlue.withOpacity(0.5)),
-          child: Table(
+          height: 500,
+          child: ListView(
             children: [
-              TableRow(children: [
-                Padding(
-                  padding: const EdgeInsets.all(8.0),
-                  child: Container(
-                    color: CupertinoColors.white,
-                    child: Center(
-                      child: Text('19'),
+              Card(
+                color: CupertinoColors.systemGrey3,
+                shape: RoundedRectangleBorder(
+                    borderRadius: BorderRadius.circular(20)),
+                child: Column(
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  children: [
+                    Padding(
+                      padding: const EdgeInsets.only(
+                          left: 10, top: 5, bottom: 5, right: 5),
+                      child: Text(
+                        '19 November 2022',
+                        style: TextStyle(
+                            fontSize: 18, fontWeight: FontWeight.bold),
+                      ),
                     ),
-                  ),
+                    Padding(
+                      padding: const EdgeInsets.all(5.0),
+                      child: Table(
+                        columnWidths: {
+                          0: FlexColumnWidth(1),
+                          1: FlexColumnWidth(1),
+                          2: FlexColumnWidth(1),
+                          3: FlexColumnWidth(1),
+                          4: FlexColumnWidth(2),
+                        },
+                        children: [
+                          columnHeaders(),
+                          userDataColumns('07:59', '200', '2', '20', 'Dead'),
+                          userDataColumns('07:59', '200', '2', '20', 'Dead'),
+                          userDataColumns('07:59', '200', '2', '20', 'Dead'),
+                          userDataColumns('07:59', '200', '2', '20', 'Dead'),
+                          userDataColumns('07:59', '200', '2', '20', 'Dead'),
+                        ],
+                      ),
+                    )
+                  ],
                 ),
-                Text(' November 2022')
-              ])
+              ),
+              Card(
+                color: CupertinoColors.systemGrey3,
+                shape: RoundedRectangleBorder(
+                    borderRadius: BorderRadius.circular(20)),
+                child: Column(
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  children: [
+                    Padding(
+                      padding: const EdgeInsets.only(
+                          left: 10, top: 5, bottom: 5, right: 5),
+                      child: Text(
+                        '20 November 2022',
+                        style: TextStyle(
+                            fontSize: 18, fontWeight: FontWeight.bold),
+                      ),
+                    ),
+                    Padding(
+                      padding: const EdgeInsets.all(5.0),
+                      child: Table(
+                        columnWidths: {
+                          0: FlexColumnWidth(1),
+                          1: FlexColumnWidth(1),
+                          2: FlexColumnWidth(1),
+                          3: FlexColumnWidth(1),
+                          4: FlexColumnWidth(2),
+                        },
+                        children: [
+                          columnHeaders(),
+                          userDataColumns('07:59', '200', '2', '20', 'Dead')
+                        ],
+                      ),
+                    )
+                  ],
+                ),
+              )
             ],
           ),
         ),
-      ]),
-    );
+      ],
+    ));
   }
+}
+
+TableRow dateHeader(int day, String monthYear) {
+  return TableRow(children: [
+    Row(children: [
+      Padding(
+        padding: const EdgeInsets.all(8.0),
+        child: Container(
+          height: 20,
+          width: 40,
+          color: CupertinoColors.systemGrey.withOpacity(0.5),
+          child: Center(
+            child: Text(day.toString()),
+          ),
+        ),
+      ),
+      Text(monthYear)
+    ])
+  ]);
+}
+
+TableRow columnHeaders() {
+  return TableRow(children: [
+    Center(
+      child: Container(
+        height: 20,
+        child: Text(
+          'Time',
+          style: TextStyle(fontWeight: FontWeight.bold),
+        ),
+      ),
+    ),
+    Center(
+      child: Text(
+        'Sugar',
+        style: TextStyle(fontWeight: FontWeight.bold),
+      ),
+    ),
+    Center(
+      child: Text(
+        'Insulin',
+        style: TextStyle(fontWeight: FontWeight.bold),
+      ),
+    ),
+    Center(
+      child: Text(
+        'Carbs',
+        style: TextStyle(fontWeight: FontWeight.bold),
+      ),
+    ),
+    Center(
+      child: Text(
+        'Comments',
+        style: TextStyle(fontWeight: FontWeight.bold),
+      ),
+    )
+  ]);
+}
+
+TableRow userDataColumns(time, sugar, insulin, carbs, comment) {
+  return TableRow(children: [
+    Center(
+        child: Container(
+      height: 20,
+      child: Text(time),
+    )),
+    Center(child: Text(sugar)),
+    Center(child: Text(insulin)),
+    Center(child: Text(carbs)),
+    Center(child: Text(comment)),
+  ]);
 }
